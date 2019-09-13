cd5042f7354ceaacb2f3b4162eef1f2316e82e32
blake3160 <55066068+blake3160@users.noreply.github.com>
2019-09-13 14:36:58 -0500

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,32 @@
+// this is the script that Blake Worked on
+
+var infoState = "1";
+
+var gabenImg = document.getElementById('gaben');
+
+gabenImg.addEventListener("dblclick", moreInfo)
+
+function moreInfo() {
+    
+    
+if (infoState == "1")
+    {
+        let newPara = document.createElement('p');
+        console.log(newPara);
+
+        let newParaNode = document.createTextNode("Fun Fact: Gaben Newell is also one of the main producers of the First Windows Operation System.");
+
+        newPara.appendChild(newParaNode);
+    
+        document.body.appendChild(newPara);
+        
+        infoState = "2"
+        alert("a fun fact was displayed below the first paragraph!");
+    }
+    else 
+        {
+            alert("sorry no more fun facts here D:");
+        }
+}
+
+// end of code
