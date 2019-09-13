2d9538cc761aff39599572d8d3b2f8bb84d95002
blake3160 <55066068+blake3160@users.noreply.github.com>
2019-09-13 14:37:00 -0500

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,16 @@
+
+//Script that Garrett worked on
+var cod = document.getElementById('basic');
+cod.addEventListener('click',addInfo);
+
+function addInfo(){
+alert("This is a screen shot from Call of Duty: Moder Warfare 3, One of the biggest FPS games of all time.")
+
+}
+
+var wolf = document.getElementById('wolf');
+wolf.addEventListener('click',addInfo2);
+
+function addInfo2(){
+  alert("This is a sceen shot from the game Wolfenstine 3-D, The first major FPS game.")
+}
