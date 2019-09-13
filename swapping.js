55595d2bee55228a9a78456406a3bf113a2a463f
blake3160 <55066068+blake3160@users.noreply.github.com>
2019-09-13 14:37:05 -0500

    Create swapping.js

---
+++ swapping.js
@@ -0,0 +1,10 @@
+/*
+    Author: Noah Gumm
+    Date: 09/04/2019
+*/
+
+//Swaps main image
+function swap_Image(elementId, imageSource) {
+    //Switch element image with id of mainImage to the imageSource parameter
+    document.getElementById(elementId).src = imageSource;
+}
