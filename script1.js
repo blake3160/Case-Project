93913d1bca4fbe9bf4bfd74296b6f7b41c85156e
GarrettBoxberger <55366172+GarrettBoxberger@users.noreply.github.com>
2019-09-15 23:45:59 -0500

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,36 +1,59 @@
 // Parker Stratton | declaring variables
-
-var factArray = ["Fun Fact 1: Gaben Newell is also one of the main producers of the First Windows Operation System.", "Fun Fact 2: Gaben does not like the number 3!", "Fun Fact 3: Gaben is sometimes called 'Lord Gaben' because he grants discounts on steam games."]
+//Blake made the stings for the array
+var factArray = ["Fun Fact 1: Gaben Newell is also one of the main producers of the First Windows Operation System.", "Fun Fact 2: Gaben does not like the number 3!", "Fun Fact 3: Gaben is sometimes called 'Lord Gaben' because he grants discounts on steam games."];
 
 // this is the script that Blake Worked on
-
-var infoState = "1";
 
 var gabenImg = document.getElementById('gaben');
 
 gabenImg.addEventListener("dblclick", moreInfo)
 
+var i = 0;
+
 function moreInfo() {
-    
-    
-if (infoState == "1")
-    {
-        let newPara = document.createElement('p');
-        console.log(newPara);
 
-        let newParaNode = document.createTextNode(factArray[0]);
+//Garrett was the creater of this dumb loop
+if (i == 0 ){
+  let newPara = document.createElement('p');
+  console.log(newPara);
 
-        newPara.appendChild(newParaNode);
-    
-        document.body.appendChild(newPara);
-        
-        infoState = "2"
-        alert("a fun fact was displayed below the first paragraph!");
-    }
-    else 
-        {
-            alert("sorry no more fun facts here D:");
-        }
+  let newParaNode = document.createTextNode(factArray[i]);
+
+
+  newPara.appendChild(newParaNode);
+  document.body.appendChild(newPara);
+  i++;
 }
+
+else if (i == 1){
+  let newPara = document.createElement('p');
+  console.log(newPara);
+
+  let newParaNode = document.createTextNode(factArray[i]);
+
+
+  newPara.appendChild(newParaNode);
+  document.body.appendChild(newPara);
+  i++;
+}
+
+else if (i == 2){
+  let newPara = document.createElement('p');
+  console.log(newPara);
+
+  let newParaNode = document.createTextNode(factArray[i]);
+
+
+  newPara.appendChild(newParaNode);
+  document.body.appendChild(newPara);
+  i++;
+}
+
+else{
+    alert("Why are you still clicking? Your just looping the same facts.");
+    i = 0
+}
+}
+
 
 // end of code
