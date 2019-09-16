b4ece6f3d776e8f785cddf113fcad90e5254c45d
blake3160 <55066068+blake3160@users.noreply.github.com>
2019-09-15 23:49:53 -0500

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,5 +1,5 @@
 // Parker Stratton | declaring variables
-//Blake made the stings for the array
+// Blake made the stings for the array
 var factArray = ["Fun Fact 1: Gaben Newell is also one of the main producers of the First Windows Operation System.", "Fun Fact 2: Gaben does not like the number 3!", "Fun Fact 3: Gaben is sometimes called 'Lord Gaben' because he grants discounts on steam games."];
 
 // this is the script that Blake Worked on
